de4fe647f66a0056ca900fe134604e7908d344a1
Alex Chan <alyxchan87@gmail.com>
2024-01-24 14:32:54 -0800

    Auto-install lsp if current buffer doesn't have one

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     -- setup treesitter
     local config = require("nvim-treesitter.configs")
     config.setup({
       ensure_installed = { "lua", "rust", "javascript" },
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
-
